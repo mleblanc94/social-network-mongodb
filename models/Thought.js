800db390e53a8e8eb27ef5f091c17e71affecef5
Michael Leblanc <mleblanc1@werfen.com>
2023-11-13 13:47:13 -0500

    finished out the models code

---
+++ models/Thought.js
@@ -0,0 +1,36 @@
+const { Schema, model } = require('mongoose');
+const reactionSchema = require('./Reaction');
+
+// Schema to create Thought model
+const thoughtSchema = new Schema(
+    {
+        thoughtText: {
+            type: String,
+            required: true,
+            max_length: 280,
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now(),
+        },
+        username: {
+            type: String,
+            required: true,
+        },
+        reactions: [reactionScema],
+    },
+    {
+        toJSON: { getters: true },
+    }
+);
+
+// Virtual to get reaction count
+
+thoughtSchema.virtual('reactionCount').get(function () {
+    return this.reactions.length;
+});
+
+// Creating the Thought model
+const Thought = model('Thought', thoughtSchema);
+
+module.exports = Thought;
